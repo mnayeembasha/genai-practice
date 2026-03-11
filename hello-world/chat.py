014f10953e3b72a28c8c649eb8631bdc9edb2363
Nayeem Basha <mnayeembasha@gmail.com>
2026-03-11 22:02:23 +0530

    chore: change base modal

--- hello-world/chat.py
+++ hello-world/chat.py
@@ -7,7 +7,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 response = client.chat.completions.create(
-    model="gpt-4.1-mini",
+    model="gpt-4.1-nano",
     messages=[
         {
             "role":"user","content":"Hi, How are you?"
